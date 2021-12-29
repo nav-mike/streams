58f1c519b583514970a69efee1807274d91fc9d5
Mikhail Navrotskii <mikhail@brella.io>
2021-12-29 15:03:49 +0200

    remove old ui

--- stream-client/src/App.tsx
+++ stream-client/src/App.tsx
@@ -6,14 +6,7 @@ import Details from "./components/details";
 function App() {
   return (
     <Container maxW="container.xl" p={0}>
-      <Flex
-        h={{ base: "auto", md: "100vh" }}
-        py={[0, 10, 20]}
-        direction={{ base: "column-reverse", md: "row" }}
-      >
-        <Details />
-        <Cart />
-      </Flex>
+      <Flex></Flex>
     </Container>
   );
 }
