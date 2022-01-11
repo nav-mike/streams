f209db4595f58aa9f821b052f53e1066bc758438
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-01-11 14:48:20 +0200

    add message data generator

--- stream_server/lib/stream_server_web/channels/room_channel.ex
+++ stream_server/lib/stream_server_web/channels/room_channel.ex
@@ -1,6 +1,16 @@
 defmodule StreamServerWeb.RoomChannel do
   use StreamServerWeb, :channel
 
+  @authors %{
+    "Dan Abramov" => "https://bit.ly/dan-abramov",
+    "Kola Tioluwani" => "https://bit.ly/tioluwani-kolawole",
+    "Kent Dodds" => "https://bit.ly/kent-c-dodds",
+    "Ryan Florence" => "https://bit.ly/ryan-florence",
+    "Prosper Otemuyiwa" => "https://bit.ly/prosper-baba",
+    "Christian Nwamba" => "https://bit.ly/code-beast",
+    "Segun Adebayo" => "https://bit.ly/sage-adebayo"
+  }
+
   @impl true
   def join("room:lobby", payload, socket) do
     if authorized?(payload) do
@@ -18,8 +28,9 @@ defmodule StreamServerWeb.RoomChannel do
   end
 
   def handle_in("new_msg", %{"body" => body}, socket) do
-    broadcast(socket, "new_msg", %{"body" => body})
-    {:reply, {:ok, %{"body" => body}}, socket}
+    mbd = message_body(body["message"])
+    broadcast(socket, "new_msg", %{"body" => mbd})
+    {:reply, {:ok, %{"body" => mbd}}, socket}
   end
 
   # It is also common to receive messages from the client and
@@ -33,5 +44,20 @@ defmodule StreamServerWeb.RoomChannel do
   # Add authorization logic here as required.
   defp authorized?(_payload) do
     true
+  end
+
+  def message_body(message) do
+    author = @authors |> Map.keys() |> Enum.random()
+    avatar = Map.get(@authors, author)
+    dt = case DateTime.now("Etc/UTC") do
+      {:ok, date_time} -> DateTime.to_iso8601(date_time)
+      {:error, _} -> "2022-01-11T11:57:09.965513Z"
+    end
+    %{
+      :author => author,
+      :authorAvatar => avatar,
+      :message => message,
+      :createdAt => dt
+    }
   end
 end
